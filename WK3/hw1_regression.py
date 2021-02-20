7a7ba4cd01906e76c7803dc9cc42f6241337efd5
richarddoss <richarddoss07195@gmail.com>
2021-02-20 17:06:39 -0500

    added active learning

--- WK3/hw1_regression.py
+++ WK3/hw1_regression.py
@@ -13,14 +13,9 @@ def part1(X_train,y_train,X_test,lambda_input):
    X = np.matrix(X_train)
    Nd = X.shape
    d = Nd[1]
-   print(d)
-  # np.multiply(X.T,X)
    y = np.matrix(y_train).T
-   print(y.shape)
    B=np.dot(X.T,y)
    A=np.linalg.inv(np.add(lambda_input*np.identity(d),np.dot(X.T,X)))
-   print(A.shape)
-   print(B.shape)
    wRR = np.dot(A,B)
    print(wRR)
   # Inverse= lambda_input*(np.identity(d))+ X_train'*X_train)i
@@ -33,10 +28,36 @@ part1(X_train,y_train,X_test,lambda_input)  # Assuming wRR is returned from the 
 
 
 ## Solution for Part 2
-#def part2():
+def part2(X_train,y_train,X_test,lambda_input,sigma2_input):
     ## Input : Arguments to the function
+    X = np.matrix(X_train)
+    sigmaInv=1/sigma2_input
+    Nd = X.shape
+    d = Nd[1]
+    y = np.matrix(y_train).T
+    XTX=np.dot(X.T,X)
+    XTy=np.dot(X.T,y)
+    lamd_i=lambda_input*np.identity(d)
+    # compute co-variance matrix of the posterior distr on the weights
+    eps = np.linalg.inv(np.add(lamd_i, sigmaInv*XTX))
+    # compute variance for each prediction over the entire X_test
+    Xtest = np.matrix(X_test)
+    TestObs = Xtest.shape
+    TestObs = TestObs[0]
+    sigmaVal = np.zeros(50)
+    print(eps.shape)
+    for x in range(TestObs):
+        #print(Xtest[x,:].T.shape)
+        #print(Xtest[x,:].shape)
+        sigmaVal[x]=sigma2_input +np.dot(np.dot( Xtest[x,:],eps),Xtest[x,:].T)
+        #print(sigma0)
+    print(sigmaVal)
+    idx=np.argsort(sigmaVal)
+    # return 10 points with the most variance to be measured for y values
+    print(idx[0:10])
+    #print(idx)
     ## Return : active, Final list of values to write in the file
-#    pass
 
-#active = part2()  # Assuming active is returned from the function
+
+part2(X_train,y_train,X_test,lambda_input,sigma2_input)  # Assuming active is returned from the function
 #np.savetxt("active_" + str(lambda_input) + "_" + str(int(sigma2_input)) + ".csv", active, delimiter=",") # write output to file
